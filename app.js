c6ab3d062e88271c2a019a4625df3587c732d7df
Rohit Bhardwaj <rohitbhardwaj@Rohits-MacBook-Air.local>
2021-07-15 13:13:17 +0530

    spelling change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const chalk = require("chalk");
 const yargs = require("yargs");
 const notes = require('./notes.js');
 
-// customising yargs version
+// customising yarg version
 yargs.version('1.1.0');
 
 // Create add command
